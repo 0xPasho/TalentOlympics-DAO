3cda5dce88fd6029137d9ef5cc5d8fa9c06ca2bb
pasho <brandon.aguilarc@gmail.com>
2024-07-16 18:40:09 -0600

    didn't added the test changes on the latest push

--- tests/dao-voting-base.ts
+++ tests/dao-voting-base.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { DaoVoting } from "../target/types/talent_olympics_dao_voting";
+import { DaoVoting } from "../target/types/dao_voting";
 import { assert } from "chai";
 import dayjs from "dayjs";
 
@@ -9,8 +9,7 @@ describe("dao-voting", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace
-    .DaoVoting as Program<DaoVoting>;
+  const program = anchor.workspace.DaoVoting as Program<DaoVoting>;
 
   const [user1, user2, user3, user4] = [
     anchor.web3.Keypair.generate(),
@@ -21,7 +20,7 @@ describe("dao-voting", () => {
 
   const id = new anchor.BN(1);
   const [proposalAccount] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("proposal"), id.toArrayLike(Buffer, "le", 8)],
+    [Buffer.from("prop"), id.toArrayLike(Buffer, "le", 8)],
     program.programId
   );
 
@@ -49,20 +48,20 @@ describe("dao-voting", () => {
         user4.publicKey,
         5 * anchor.web3.LAMPORTS_PER_SOL
       );
-      await provider.connection.confirmTransaction(tx3);
+      await provider.connection.confirmTransaction(tx4);
     }
   });
 
   it("Should init a proposal successfully", async () => {
     const tx = await program.methods
-      .initProposal(
+      .createProposal(
         id,
         "Give 1000 USDC to the winner",
         maximumVote,
         new anchor.BN(endTime)
       )
       .accountsPartial({
-        signer: user1.publicKey,
+        user: user1.publicKey,
         proposal: proposalAccount,
       })
       .signers([user1])
@@ -72,8 +71,8 @@ describe("dao-voting", () => {
 
     const proposal = await program.account.proposal.fetch(proposalAccount);
     assert.equal(proposal.id.toNumber(), id.toNumber());
-    assert.equal(proposal.maxVotes.toNumber(), maximumVote.toNumber());
-    assert.equal(proposal.votesFor.toNumber(), 0);
+    assert.equal(proposal.maxVotesAllowed.toNumber(), maximumVote.toNumber());
+    assert.equal(proposal.votesAgainst.toNumber(), 0);
     assert.equal(proposal.votesAgainst.toNumber(), 0);
 
     console.log("Init proposal tx", tx);
@@ -81,9 +80,9 @@ describe("dao-voting", () => {
 
   it("Should vote on a proposal successfully", async () => {
     const tx1 = await program.methods
-      .vote(id, true)
+      .submitVote(id, true)
       .accountsPartial({
-        voter: user2.publicKey,
+        user: user2.publicKey,
         proposal: proposalAccount,
       })
       .signers([user2])
@@ -94,8 +93,8 @@ describe("dao-voting", () => {
     console.log("user2 Vote for", tx1);
 
     const tx2 = await program.methods
-      .vote(id, false)
-      .accountsPartial({ voter: user3.publicKey, proposal: proposalAccount })
+      .submitVote(id, false)
+      .accountsPartial({ user: user3.publicKey, proposal: proposalAccount })
       .signers([user3])
       .rpc();
 
@@ -105,16 +104,16 @@ describe("dao-voting", () => {
 
     const proposal = await program.account.proposal.fetch(proposalAccount);
 
-    assert.equal(proposal.votesFor.toNumber(), 1);
+    assert.equal(proposal.votesInFavor.toNumber(), 1);
     assert.equal(proposal.votesAgainst.toNumber(), 1);
   });
 
   it("Should vote fail if maximum votes reached", async () => {
     try {
       await program.methods
-        .vote(id, true)
+        .submitVote(id, true)
         .accountsPartial({
-          voter: user4.publicKey,
+          user: user4.publicKey,
           proposal: proposalAccount,
         })
         .signers([user4])
@@ -129,9 +128,9 @@ describe("dao-voting", () => {
   it("Should creator close proposal fail when proposal not expired", async () => {
     try {
       const tx = await program.methods
-        .closeProposal(id)
+        .terminateProposal(id)
         .accountsPartial({
-          signer: user1.publicKey,
+          user: user1.publicKey,
           proposal: proposalAccount,
         })
         .signers([user1])
@@ -146,9 +145,9 @@ describe("dao-voting", () => {
     await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const tx = await program.methods
-      .closeProposal(id)
+      .terminateProposal(id)
       .accountsPartial({
-        signer: user1.publicKey,
+        user: user1.publicKey,
         proposal: proposalAccount,
       })
       .signers([user1])
